8f2b93820feac4b33ad5ab870437a8c4b7d527c5
susmithagankidi <gankidisusmitha@gmail.com>
2019-11-20 13:20:33 -0500

    test

---
+++ test.sh
@@ -0,0 +1,9 @@
+if  grep 'lz_isr_fetch_user_data_function04' file1.txt; then
+  cd lz_isr_fetch_user_data_function04
+  npm test
+elif grep 'dev_isr_fetch_mqa_active_alarms_per_site' file1.txt; then
+  cd dev_isr_fetch_mqa_active_alarms_per_site
+  npm test
+ else
+	 echo "no changes to folders"
+fi
