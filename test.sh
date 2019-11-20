889ae5a823cf3598ddc460d2c3e685c6ab977905
susmithagankidi <gankidisusmitha@gmail.com>
2019-11-20 17:55:08 -0500

    c

--- test.sh
+++ test.sh
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-if '[$(grep 'lz_isr_fetch_user_data_function04' file1.txt) && $(grep 'lz_isr_fetch_user_data_function04' file1.txt)]'; then 
-=======
-if [$(grep 'lz_isr_fetch_user_data_function04' file1.txt) && $(grep 'lz_isr_fetch_user_data_function04' file1.txt)]; then 
->>>>>>> 214632faa753c8c5ea3b6f14fb1d2801f57f033b
+if '[$(grep 'lz_isr_fetch_user_data_function04' file1.txt) && $(grep 'lz_isr_fetch_user_data_function04' file1.txt)]'; then
 cd dev_isr_fetch_mqa_active_alarms_per_site
  npm test
  cd ../lz_isr_fetch_user_data_function04
